6b622a0490218cc777a57413f209862c7926b9bc
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-07-12 17:15:55 +0100

    first commit

---
+++ src/main/java/com/qa/dfemytunes/DfeMyTunesApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfemytunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeMyTunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeMyTunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dfemytunes/DfeMyTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfemytunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeMyTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
