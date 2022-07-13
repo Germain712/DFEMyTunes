e7046fabfe6712546b5887709bb26bcf25b2a383
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-07-13 09:49:23 +0100

    setting columns up

---
+++ src/main/java/com/qa/dfemytunes/entities/Artist.java
@@ -0,0 +1,18 @@
+package com.qa.dfemytunes.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Artist {
+	
+	private long id;
+	
+	private String title;
+	
+	private String artistName;
+	
+	private String album;
+	
+	private String genre;
+
+}
