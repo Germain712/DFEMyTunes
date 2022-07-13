5ef89cd85fe39ee0ce3a57e76c44172bbe356996
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-07-13 09:57:48 +0100

    added Getters & Setters

--- src/main/java/com/qa/dfemytunes/entities/Artist.java
+++ src/main/java/com/qa/dfemytunes/entities/Artist.java
@@ -15,4 +15,46 @@ public class Artist {
 	
 	private String genre;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtistName() {
+		return artistName;
+	}
+
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
+
 }
