eae696e484e68e8922a18f920daa1b3e57457bb7
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-07-13 10:14:24 +0100

    added private key & setup auto-increment

--- src/main/java/com/qa/dfemytunes/entities/Artist.java
+++ src/main/java/com/qa/dfemytunes/entities/Artist.java
@@ -1,10 +1,15 @@
 package com.qa.dfemytunes.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Artist {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	private String title;
